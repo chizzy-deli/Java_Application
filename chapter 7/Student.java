52cdb2e2914d3e9c17d61006a949ad1c1b49f94a
chizzy-deli <delightchizzy65@gmail.com>
2025-01-28 18:24:22 -0600

    chapter 6 assignment

---
+++ chapter 7/Student.java
@@ -0,0 +1,14 @@
+public class Student{
+	int studentID = 101;
+    String name = "John";
+	int age = 15;
+	String gender = "Male";
+	
+	public void study(){
+		System.out.println(" I am learning Java");
+			
+	}
+}
+public class DemoStudent{
+	public static void main(String[] args){
+		
